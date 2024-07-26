febeb38670b03501c4e1cdf9720c32ac643dd6e4
nick0413 <68749910+nick0413@users.noreply.github.com>
2024-07-26 11:23:31 -0600

    Refactored code for tribometer

--- Tribometer_data/tribo.py
+++ Tribometer_data/tribo.py
@@ -1,11 +1,21 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from scipy.signal import savgol_filter
 import re
 import scipy.signal as signal
 
 
+'''
+	Tribo is a work in progress module that is designed to process tribology data from a tribometer.
+
+	Authored by: 	Nicolas Cordoba 	|| cordobagarzonnicolas@gmail.com
+					Albert Kalayil 		|| 
+
+'''
+
+def split_string(s):
+    return re.split('[-_]', s)
 
 labels = {
     "upward_sections": "Forward Movement CoF",
@@ -199,9 +209,146 @@ def calculate_CoF(upward_sections, forward_friction, downward_sections, backward
 
 	return CoF_avg
 
+class Tribo_file:
+	def __init__(self,file_folder, file_name, outlier=False):
+		self.file_name = file_name
+		self.file_folder = file_folder
+		self.percent,self.name,self.force,self.speed,self.test = split_string(self.file_name)
+		self.outlier=outlier
+		
+		self.load_data()
+
+	def load_data(self):
+		self.data,self.xpos,self.Fx,self.Fz = get_data(self.file_folder+"\\"+self.file_name,"all")
+		
+
+	def process_data(self,cutoff=0.1):
+
+		self.xpos_peaks, self.xpos_valleys = find_x_segments(self.xpos)
+
+		segments=segment_data(self.xpos, self.xpos_peaks, self.xpos_valleys,self.Fx,self.Fz)
+
+		self.upward_sections, self.forward_friction, self.downward_sections, self.backward_friction=segments
+
+		self.CoF_array = calculate_CoF(self.upward_sections, self.forward_friction, self.downward_sections, self.backward_friction,cutoff)
+
+		self.CoF_avg=np.average(self.CoF_array)
+		self.CoF_std=np.std(self.CoF_array, ddof=1)
+
+		pad_length = 3000 - len(self.CoF_array)
+		if pad_length > 0:
+			padded_array = np.pad(self.CoF_array, (0, pad_length), 'constant', constant_values=self.CoF_avg)
+		else:
+			padded_array = self.CoF_array
+
+		self.CoF_array=padded_array
+
+def load_files(files_in_folder: list,folder: str,outlier_tests: dict={}):
+	Tribo_files = []
+	complete_CoF_df = pd.DataFrame()
+	for file_name in files_in_folder:
+		if  outlier_tests:
+			if file_name in outlier_tests[folder]:
+			
+				file_n=Tribo_file(folder, file_name, outlier=True)
+			
+		else:
+			file_n=Tribo_file(folder, file_name)
+		file_n.process_data(0.2)
+		Tribo_files.append(file_n)
+		
+		complete_CoF_df[file_name]=file_n.CoF_array	
+
+	return Tribo_files, complete_CoF_df
+
+def smoothing_df(data_array: pd.DataFrame):
+	sg_smoothing = pd.DataFrame()
+	for column in data_array.columns:
+		sg_smoothing[column]=savgol_filter(data_array[column], 100, 2)
+
+	return sg_smoothing
+
+def get_speeds_and_names_in_folder(files_in_folder: list, Tribo_files_list: list):
+	speeds=[]
+	names=[]
+
+	for file in Tribo_files_list:
+		if file.speed not in speeds:
+			speeds.append(file.speed)
+
+		if file.name not in names:
+			names.append(file.name)
+
+	return speeds, names
+
+def remove_outliers(files: list, excluded_speeds: list =[], verbose: bool = False):
+	rows=[]
+	test_list = [] 
+	count1 = 0
+	for file in files:
+		if file.outlier:
+			count1 += 1
+		elif file.speed in excluded_speeds:
+			continue 
+		else:
+			data_to_append = [ file.name,  file.CoF_avg,  file.CoF_std,file.speed, file.percent, file.force,file.test]
+			rows.append(data_to_append)
+
+		if verbose: 
+			print('Outliers num: ', count1)
+			print('Outlier Files found: ', test_list)
+	
+	return rows
+
+def sort_dfs_by_speed(speeds: list, df: pd.DataFrame):
+	'''
+	This returns a list of dataframes sorted by speed
+	'''
+	
+	speed_sheets=[]
+	for speed in speeds:
+		speed_sheet=df[df['Speed']==speed]
+		print("speed sheet\n",speed_sheet)
+		speed_sheets.append(speed_sheet)
+
+	return speed_sheets
+
+def export_excel_results(speed_sheets_df,speeds,sg_smoothing_df,folder,complete_CoF,verbose: bool = False):
+	with pd.ExcelWriter(f'{folder}.xlsx') as writer: 
+		for df,speed  in zip(speed_sheets_df,speeds): 
+			rows_total=[]
+			total_dfs=[]
+
+			for name in df['Name'].unique():	
+				for force in df['Force'].unique(): 
+					
+					if verbose: print(f"----------{name}\t{force}------------")
+
+					df2=df[(df['Name']==name) & (df['Force']==force)]
+					# Here we want to combine all of the tests for the same name and force
+
+					if df2.empty: 
+						continue #it is possible that there are no tests for a given combination of name and force
+
+					CoF_total_avg=np.average(df2['CoF_avg'])
+					CoF_total_std=np.sqrt(np.sum(df2['CoF_std'].to_numpy()**2))/len(df2['CoF_std'])
+					
+
+					row=[name,CoF_total_avg,CoF_total_std,speed,df2.get("Force").iloc[0],df2.get("Percent").iloc[0]]
+					rows_total.append(row)
+			df=pd.DataFrame(rows_total,columns=['Name','CoF_avg','CoF_std','Speed', 'Force','Percent'])
+			total_dfs.append(df)
+			df.to_excel(writer, sheet_name=speed, index=False)
 
 
-# ----------------- Example -----------------
+		complete_df=pd.concat(total_dfs)
+		complete_df.to_excel(writer, sheet_name='Total', index=False)
+		complete_CoF.to_excel(writer, sheet_name='CoF', index=True)
+		sg_smoothing_df.to_excel(writer, sheet_name='Smoothed CoF', index=True)
+
+
+
+# ----------------- MARK: Ex:loading data -----------------
 
 # file_name='DATA\\OA-10_10N_100mms_test4_May8'
 # data,xpos,Fx,Fz=get_data(file_name,"all", True)
